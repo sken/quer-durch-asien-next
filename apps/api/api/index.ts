92d3f88b0d404c3c9a4e4ce96542cba0f15e5bf7
sken <skendlbacher@gmail.com>
2025-08-07 15:17:38 +0200

    Rename serverless.ts to index.ts and update Vercel configuration for API routing

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -4,10 +4,15 @@ const app = Fastify({
     logger: false,
 });
 
+/*
 app.register(import("../src/app"), {
     prefix: '/'
 });
 
+ */
+app.get('/', async (req, reply) => {
+    return reply.status(200).type('text/html').send("test")
+})
 export default async (req: any, res: any) => {
     await app.ready();
     app.server.emit('request', req, res);
